5baad7f8e84816b6515d7fd90aa9fab08333e6b3
myneilswart <143065844+myneilswart@users.noreply.github.com>
2026-07-04 20:54:45 +0200

    Update file_finder.py

--- file_finder.py
+++ file_finder.py
@@ -6,7 +6,7 @@ No files are copied or moved — only their URIs are collected.
 
 Results are:
   • Printed to the terminal
-  • Saved as a clickable HTML report (default: Desktop)
+  • Saved as a clickable HTML report and/or txt file to selected location (default save to Desktop)
     Each link opens the file's folder in File Explorer, not the file itself.
 
 Hidden file support:
